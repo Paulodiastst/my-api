ea599bb12905b183280882ff188a2470ff31e919
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-07-11 21:35:56 -0300

    Tratamento de exceções

---
+++ src/test/java/com/paulo/myapi/services/exceptions/ObjectNotFoundExceptionTest.java
@@ -0,0 +1,14 @@
+package com.paulo.myapi.services.exceptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ObjectNotFoundExceptionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
