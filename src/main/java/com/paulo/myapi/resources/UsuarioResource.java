fde1d739dfb0f29447b6d06676e6f56b358ccd2c
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-07-11 22:48:37 -0300

    Post usuarios

--- src/main/java/com/paulo/myapi/resources/UsuarioResource.java
+++ src/main/java/com/paulo/myapi/resources/UsuarioResource.java
@@ -1,16 +1,18 @@
 package com.paulo.myapi.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.paulo.myapi.domain.Usuario;
 import com.paulo.myapi.services.UsuarioService;
@@ -38,6 +40,13 @@ public class UsuarioResource {
 	public ResponseEntity<Usuario> update(@PathVariable Integer id, @RequestBody Usuario obj) {
 		Usuario newObj = service.Upadate(id, obj);
 		return ResponseEntity.ok().body(newObj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> create(@RequestBody Usuario obj) {
+		Usuario newObj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
